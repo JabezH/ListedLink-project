0254e84d79947fe3e6047ca278e8b4a48ed60ecd
JabezH <hintonjabez@gmail.com>
2024-05-13 22:39:30 -0400

    listedLink project

---
+++ doubly-linked-list.js
@@ -0,0 +1,63 @@
+class DoublyLinkedListNode {
+  constructor(val) {
+    this.value = val;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    // Your code here 
+    this.head = null
+    this.tail = null
+    this.length = 0
+  }
+
+  addToHead(val) {
+    // Your code here  
+    let newNode = new DoublyLinkedListNode(val) 
+     this.length++
+     if(this.head === null) {
+      this.head = newNode
+      this.tail = newNode
+     } else {
+      newNode.next = this.head
+      this.head.prev = newNode
+      this.head = newNode
+
+    } 
+    return this;
+  }
+
+  
+  addToTail(val) {
+    // Your code here 
+    let newNode = new DoublyLinkedListNode(val);
+    this.length++;
+     if(this.tail === null) {
+      this.head = newNode;
+      this.tail = newNode;
+     } else {
+     newNode.prev = this.tail;
+     this.tail.next = newNode;
+     this.tail = newNode;
+  }
+      return this;
+}
+
+
+  // You can use this function to help debug
+  print() {
+    let current = this.head;
+
+    while (current) {
+      process.stdout.write(`${current.value} -> `);
+      current = current.next;
+    }
+
+    console.log("NULL");
+  }
+}
+
+module.exports = DoublyLinkedList;
